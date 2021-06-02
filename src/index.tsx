8bb7be0235fbf1a754494ddb58370bb1e4a134cd
Дюбанова Мария Борисовна <dyubanova.mb@esphere.ru>
2021-06-03 02:02:05 +0300

    add card and main-page

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  CARD_COUNT: 5,
+  PLACES_TO_STAY: 200,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      cardCount={Setting.CARD_COUNT}
+      placesToStay={Setting.PLACES_TO_STAY}
+    />
   </React.StrictMode>,
   document.getElementById('root'),
 );
