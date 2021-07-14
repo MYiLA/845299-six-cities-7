673a79a573e871bbd212aba31b45eb3c8cb0eeb4
Дюбанова Мария Борисовна <dyubanova.mb@esphere.ru>
2021-07-15 00:05:17 +0300

    fix sorting

--- src/utils/sorting.ts
+++ src/utils/sorting.ts
@@ -1,8 +1,8 @@
 import { SortingType } from '../const';
 import { Hotel } from '../data-type';
 
-const sortByPriceHighToLow = (hotel1: Hotel, hotel2: Hotel): number => hotel1.price - hotel2.price;
-const sortByPriceLowToHigh = (hotel1: Hotel, hotel2: Hotel): number => hotel2.price - hotel1.price;
+const sortByPriceHighToLow = (hotel1: Hotel, hotel2: Hotel): number => hotel2.price - hotel1.price;
+const sortByPriceLowToHigh = (hotel1: Hotel, hotel2: Hotel): number => hotel1.price - hotel2.price;
 const sortByRatedFirst = (hotel1: Hotel, hotel2: Hotel): number => hotel2.rating - hotel1.rating;
 
 type Sorting = {
