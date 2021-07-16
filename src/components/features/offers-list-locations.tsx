1a414a925ae331969eac9893bd9aa52769726fb5
Дюбанова Мария Борисовна <dyubanova.mb@esphere.ru>
2021-07-17 01:08:16 +0300

    fix offers list location

--- src/components/features/offers-list-locations.tsx
+++ src/components/features/offers-list-locations.tsx
@@ -1,6 +1,5 @@
 import { PropsWithChildren, ReactElement } from "react";
 import { Link } from 'react-router-dom';
-import { AppRoute } from "../../const";
 import { Hotel } from "../../data-type";
 import { getRoute } from "../../utils/common";
 import CardFavorites from './card/card-favorites';
@@ -9,62 +8,41 @@ interface OffersListLocationParams {
   hotels: Hotel[],
 }
 
-interface LocationHotels {
-  cityName: string,
-  hotels: Hotel[],
+interface Location {
+  [key: string]: Hotel[];
 }
 
 function OffersListLocations(params: PropsWithChildren<OffersListLocationParams>): ReactElement {
   const { hotels } = params;
-  const locationHotels: LocationHotels | any[] = [];
-  console.log(hotels)
-  hotels.forEach((hotel) => {
-    // каждый отель нужно отсортировать по локациям.
-    const isLocationFind = locationHotels.find((location) => location.cityName === hotel.city.name);
 
-    if (isLocationFind) {
-      // если локация уже есть
-      // пушим отель в созданную локацию
-      locationHotels.forEach((location) => {
-        if (location.cityName === hotel.city.name) {
-          location.hotels.push(hotel);
-          return
-        }
-      })
-      return
+  const locationHotels = hotels.reduce((acc: Location, hotel: Hotel) => {
+    const cityName = hotel.city.name;
+
+    if (cityName in acc) {
+      acc[cityName].push(hotel)
     }
-    // если локация отсутствует
-    // создаём локацию
-    locationHotels.push({
-      cityName: hotel.city.name,
-      hotels: []
-    })
-    // пушим отель в созданную локацию
-    locationHotels.forEach((location) => {
-      if (location.cityName === hotel.city.name) {
-        location.hotels.push(hotel);
-        return
-      }
-    })
-    return
-  })
+    else {
+      acc[cityName] = [hotel]
+    }
 
-  console.log(locationHotels);
+    return acc
+  }, {})
+
   return (
     <section className="favorites">
       <h1 className="favorites__title">Saved listing</h1>
       <ul className="favorites__list">
-        {locationHotels.map((location)=>(
+        {Object.entries(locationHotels).map((location)=>(
           <li className="favorites__locations-items">
             <div className="favorites__locations locations locations--current">
               <div className="locations__item">
-                <Link className="locations__item-link" to={getRoute(location.cityName)}>
-                  <span>{location.cityName}</span>
+                <Link className="locations__item-link" to={getRoute(location[0])}>
+                  <span>{location[0]}</span>
                 </Link>
               </div>
             </div>
             <div className="favorites__places">
-              {location.hotels.map((hotel: Hotel) => (
+              {location[1].map((hotel: Hotel) => (
                 <CardFavorites
                   key={hotel.id}
                   cardData={hotel}
