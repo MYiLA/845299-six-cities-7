ac1268123fb70baea9f50ba4fee890b1b66f7236
Дюбанова Мария Борисовна <dyubanova.mb@esphere.ru>
2021-07-16 17:44:13 +0300

    grupped favorites offers in cities

--- src/components/features/offers-list-locations.tsx
+++ src/components/features/offers-list-locations.tsx
@@ -9,47 +9,70 @@ interface OffersListLocationParams {
   hotels: Hotel[],
 }
 
+interface LocationHotels {
+  cityName: string,
+  hotels: Hotel[],
+}
+
 function OffersListLocations(params: PropsWithChildren<OffersListLocationParams>): ReactElement {
   const { hotels } = params;
+  const locationHotels: LocationHotels | any[] = [];
+  console.log(hotels)
+  hotels.forEach((hotel) => {
+    // каждый отель нужно отсортировать по локациям.
+    const isLocationFind = locationHotels.find((location) => location.cityName === hotel.city.name);
 
+    if (isLocationFind) {
+      // если локация уже есть
+      // пушим отель в созданную локацию
+      locationHotels.forEach((location) => {
+        if (location.cityName === hotel.city.name) {
+          location.hotels.push(hotel);
+          return
+        }
+      })
+      return
+    }
+    // если локация отсутствует
+    // создаём локацию
+    locationHotels.push({
+      cityName: hotel.city.name,
+      hotels: []
+    })
+    // пушим отель в созданную локацию
+    locationHotels.forEach((location) => {
+      if (location.cityName === hotel.city.name) {
+        location.hotels.push(hotel);
+        return
+      }
+    })
+    return
+  })
+
+  console.log(locationHotels);
   return (
     <section className="favorites">
       <h1 className="favorites__title">Saved listing</h1>
       <ul className="favorites__list">
-        <li className="favorites__locations-items">
-          <div className="favorites__locations locations locations--current">
-            <div className="locations__item">
-              <Link className="locations__item-link" to={getRoute(AppRoute.DEFAULT_CITY)}>
-                <span>Amsterdam</span>
-              </Link>
+        {locationHotels.map((location)=>(
+          <li className="favorites__locations-items">
+            <div className="favorites__locations locations locations--current">
+              <div className="locations__item">
+                <Link className="locations__item-link" to={getRoute(location.cityName)}>
+                  <span>{location.cityName}</span>
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="favorites__places">
-            {hotels.map((item) => (
-              <CardFavorites
-                key={item.id}
-                cardData={item}
-              />
-            ))}
-          </div>
-        </li>
-        <li className="favorites__locations-items">
-          <div className="favorites__locations locations locations--current">
-            <div className="locations__item">
-              <Link className="locations__item-link" to={getRoute(AppRoute.OFFER)}>
-                <span>Cologne</span>
-              </Link>
+            <div className="favorites__places">
+              {location.hotels.map((hotel: Hotel) => (
+                <CardFavorites
+                  key={hotel.id}
+                  cardData={hotel}
+                />
+              ))}
             </div>
-          </div>
-          <div className="favorites__places">
-            {hotels.map((item) => (
-              <CardFavorites
-                key={item.id}
-                cardData={item}
-              />
-            ))}
-          </div>
-        </li>
+          </li>
+        ))}
       </ul>
     </section>
   )
