550f5da2449e85d617110909b02f47aba52c6dc2
Дюбанова Мария Борисовна <dyubanova.mb@esphere.ru>
2021-07-19 04:05:18 +0300

    fix naming

--- src/services/rtk-api.ts
+++ src/services/rtk-api.ts
@@ -9,7 +9,7 @@ import {
 import { adaptHotelsToClient, adaptHotelIdToClient } from '../utils/adapters/adapt-hotels';
 import adaptCommentsToClient from '../utils/adapters/adapt-comments';
 import adaptLoginToClient from '../utils/adapters/adapt-login';
-import { APIRoute } from '../const';
+import { ApiRoute } from '../const';
 
 const BACKEND_URL = 'https://7.react.pages.academy/six-cities';
 const REQUEST_TIMEOUT = 5000;
@@ -51,29 +51,29 @@ export const api = createApi({
   tagTypes: ['favorite', 'comment'],
   endpoints: (builder) => ({
     getHotels: builder.query<Hotel[], void>({
-      query: () => APIRoute.HOTELS,
+      query: () => ApiRoute.HOTELS,
       providesTags: ['favorite'],
       transformResponse: (data: any) => adaptHotelsToClient(data),
     }),
     getHotelId: builder.query<Hotel, number>({
-      query: (id) => `${APIRoute.HOTELS}/${id}`,
+      query: (id) => `${ApiRoute.HOTELS}/${id}`,
       providesTags: ['favorite'],
       transformResponse: (data: any) => adaptHotelIdToClient(data),
     }),
     getHotelIdNearby: builder.query<Hotel[], number>({
-      query: (id) => `${APIRoute.HOTELS}/${id}/nearby`,
+      query: (id) => `${ApiRoute.HOTELS}/${id}/nearby`,
       providesTags: ['favorite'],
       transformResponse: (data: any) => adaptHotelsToClient(data),
     }),
 
     getFavorites: builder.query<Hotel[], void>({
-      query: () => APIRoute.FAVORITE,
+      query: () => ApiRoute.FAVORITE,
       providesTags: ['favorite'],
       transformResponse: (data: any) => adaptHotelsToClient(data),
     }),
     postFavoriteStatus: builder.mutation<Hotel, {id: number, status: number}>({
       query: ({ id, status }) => ({
-        url: `${APIRoute.FAVORITE}/${id}/${status}`,
+        url: `${ApiRoute.FAVORITE}/${id}/${status}`,
         method: 'POST',
       }),
       invalidatesTags: ['favorite'],
@@ -81,13 +81,13 @@ export const api = createApi({
     }),
 
     getComments: builder.query<CommentGet[], number>({
-      query: (id) => `${APIRoute.COMMENTS}/${id}`,
+      query: (id) => `${ApiRoute.COMMENTS}/${id}`,
       providesTags: ['comment'],
       transformResponse: (data: any) => adaptCommentsToClient(data),
     }),
     postComment: builder.mutation<CommentGet[], {id: number, body: CommentPost}>({
       query: ({ id, body }) => ({
-        url: `${APIRoute.COMMENTS}/${id}`,
+        url: `${ApiRoute.COMMENTS}/${id}`,
         method: 'POST',
         body,
       }),
@@ -95,19 +95,19 @@ export const api = createApi({
     }),
 
     getLogin: builder.query<LoginGet, void>({
-      query: () => APIRoute.LOGIN,
+      query: () => ApiRoute.LOGIN,
       transformResponse: (data: any) => adaptLoginToClient(data),
     }),
     postLogin: builder.mutation<LoginGet, LoginPost>({
       query: (body) => ({
-        url: APIRoute.LOGIN,
+        url: ApiRoute.LOGIN,
         method: 'POST',
         body,
       }),
     }),
     deleteLogout: builder.mutation<void, void>({
       query: () => ({
-        url: APIRoute.LOGOUT,
+        url: ApiRoute.LOGOUT,
         method: 'DELETE',
       }),
     }),
